0afe1532feeb1606b0082232bf56be478f9f598e
Anouar Rfifa <anouarrafifa99@gmail.com>
2023-12-14 18:53:28 +0100

    fixed some issues with  terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -157,7 +157,6 @@ resource "azurerm_mssql_database" "myapp_sql_database" {
   server_id      = azurerm_mssql_server.myapp_sql_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
